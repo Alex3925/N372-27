2fd3eb5fb3713a4f752b06f043ac864949028525
Alex3925 <120993369+Alex3925@users.noreply.github.com>
2024-09-01 21:18:29 +0800

    Update leaveNoti.js

--- events/leaveNoti.js
+++ events/leaveNoti.js
@@ -16,7 +16,7 @@ module.exports = {
         logMessageData.leftParticipantFbId === api.getCurrentUserID()
       ) {
         api.changeNickname(
-          `[ ${prefix} ]: NashBoT`,
+          `[ ${prefix} ]: ChatBotV2`,
           threadID,
           api.getCurrentUserID()
         );
